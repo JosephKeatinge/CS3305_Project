e2a05e339c9b2d03738d82dc99d6140f719421e5
Adam Egan <115359356@umail.ucc.ie>
2018-01-23 14:51:49 +0000

    Update server.js

--- server.js
+++ server.js
@@ -19,42 +19,6 @@ server.listen(5000, function() {
  //Add the WebSocket handlers
 var players = {};
 io.on('connection', function(socket) {
-  socket.on('new player', function(data) {
-
-    console.log(data);
-    players[socket.id] = {
-      x: data.x,
-      y: data.y
-    };
-    console.log(players);
-  });
-  socket.on('position', function(data) {
-     for(var id in players){
-       var player=players[id];
-       player.x=data.x;
-       player.y=data.y;
-
-     }// Dependencies
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var socketIO = require('socket.io');
-var app = express();
-var server = http.Server(app);
-var io = socketIO(server);
-app.set('port', 5000);
-app.use('/static', express.static(__dirname + '/static'));
-// Routing
-app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, 'index.html'));
-});
-// Starts the server.
-server.listen(5000, function() {
-  console.log('Starting server on port 5000');
-});
- //Add the WebSocket handlers
-var players = {};
-io.on('connection', function(socket) {
   //A player has connected and sent there initial x,y postion
   socket.on('new player', function(data) {
     //Server stores this in a dictionary with the socket id as the key and player object
