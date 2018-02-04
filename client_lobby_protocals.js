d39f3cdf252b1f1323adddcfc0d104df15514e99
Sam <samdrugan@gmail.com>
2018-02-04 17:53:07 +0000

    added protocals to send to the server for from the client added on recieve functions to handle protocals form the server

---
+++ client_lobby_protocals.js
@@ -0,0 +1,66 @@
+var in_lobby = false
+
+/*unction to join the selected lobby
+ *
+ *@param lobby_id the unique id of the desired lobby 
+ *@param socket the current open socket to the server
+ */
+function join_lobby(lobby_id, socket){
+	socket.emit("join_lobby",lobby_id)
+}
+
+/*function to go back to the list of lobbies
+ *
+ *@param lobby_id the unique id of the lobby you are in
+ *@param socket the current open socket to the server
+ */
+function leave_lobby(lobby_id,socket){
+  socket.emit("join_lobby",lobby_id)
+}
+
+/*function to create a new lobby
+ *
+ *@param socket the current open socket to the server
+ */
+function create_lobby(socket){
+	socket.emit("create_lobby")
+}
+
+/*function to start the game in the current lobby
+ *
+ *@param socket the current open socket to the server
+ */
+function start_game(socket){
+  socket.emit("startGame")
+}
+
+/*function to get the list of current lobbies on the server
+ *
+ *@param socket the current open socket to the server
+ */
+function request_lobbies(socket){
+  socket.emit("requestLobbys")
+}
+
+/*processes the info recieved from the player about a specific lobby
+ *
+ *@param lobby_info a lobby json object that holds all the required info
+ */
+socket.on("requestInfo",lobby_info){
+  //pass
+}
+
+/*notification for all players that the game is beginning
+ */
+socket.on("startGame"){
+  //pass
+}
+
+/*procceses the info recieved for all open lobbies on the server
+ */
+socket.on("requestLobbies",function(data)){
+  lobbies_info = JSON.parse(lobbies)
+  
+}
+
+
