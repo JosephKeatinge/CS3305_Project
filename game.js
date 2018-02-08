af11edaf6208411237de44773a68fd07379d3c91
Darragh McMahon <darragh.mcmahon97@gmail.com>
2018-02-08 14:37:06 +0000

    Added socket.on('begingame')  to start game

--- game.js
+++ game.js
@@ -1,55 +1,61 @@
+//Game Logic
+
 //updateGame function
 //drawGame function
 //startGame function
 //endState function -Remove Event Listeners
 //instantiate player objects
 //Server stuff is commented out for now
+socket.on('begingame', function (gameid) {
+    var game_id = gameid;
+    var canvas = document.getElementById('canvas');
+    startGame();
+    function startGame() {
+        bullets = new Bullet(3, 3, 10);
+        player = new Player(250, 350, 32, 32, 32, "/static/hero.png");
+        proxy = new Proxy(socket, game_id);
 
 
+        //A map function
+        loadImages();
+        playerReset(player);
+        //end
+        player.imageLoad();
 
-function startGame(){
-  bullets=new Bullet(3,3,10);
-  player=new Player(250,350,32,32,32,"/static/hero.png");
-  proxy=new Proxy(socket);
-
-
-  //A map function
-  loadImages();
-  playerReset(player);
-  //end
-  player.imageLoad();
-
-  window.addEventListener("keydown", player.activate, false);
-  window.addEventListener("keyup", player.deactivate, false);
-  canvas.addEventListener('mousemove', mouseMove, true);
-  canvas.addEventListener("click", function() {
-    bullets.create(mouseX, mouseY, player.x, player.y);
-    //bullets.list.forEach( function(bullet, j) {
-      //socket.emit('shoot',bullet);
-    //});
-  });
-}
+        window.addEventListener("keydown", player.activate, false);
+        window.addEventListener("keyup", player.deactivate, false);
+        canvas.addEventListener('mousemove', mouseMove, true);
+        canvas.addEventListener("click", function () {
+            bullets.create(mouseX, mouseY, player.x, player.y);
+            //bullets.list.forEach( function(bullet, j) {
+            //socket.emit('shoot',bullet);
+            //});
+        });
+    }
 
 
-function updateGame(){
-    player.move();
-    drawGame();
-    bullets.move();
-    /*
-    socket.on('heartbeat', function(data) {
-            otherPlayers=data;
-    });
-    socket.on('bullets',function(bullets){
-             allBullets=bullets;
-    });*/
-    //Send player position if they have moved
-    if(player.hasMoved()){proxy.sendPos(player);}
-}
+    function updateGame() {
+        player.move();
+        drawGame();
+        bullets.move();
+        /*
+        socket.on('heartbeat', function(data) {
+                otherPlayers=data;
+        });
+        socket.on('bullets',function(bullets){
+                 allBullets=bullets;
+        });*/
+        //Send player position if they have moved
+        if (player.hasMoved()) { proxy.sendPos(player); }
+    }
 
-function drawGame(){
-    drawMap();
-    player.draw();
-    bullets.draw();
-    //drawOtherPlayer();
-    //drawOtherBulelts();
-}
+    function drawGame() {
+        drawMap();
+        player.draw();
+        bullets.draw();
+        //drawOtherPlayer();
+        //drawOtherBulelts();
+    }
+
+});
+
