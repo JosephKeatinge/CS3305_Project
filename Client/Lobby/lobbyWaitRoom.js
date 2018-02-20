5b411cc0387d5fa5026ba984ed78079baf5061fb
fortythreesam <samdrugan@gmail.com>
2018-02-20 11:14:22 +0000

    Added displaying of names in wait room

--- Client/Lobby/lobbyWaitRoom.js
+++ Client/Lobby/lobbyWaitRoom.js
@@ -1,22 +1,20 @@
 var lobbyWaitRoomNumberOfPlayers = 0;
-
 function startLobbyWaitRoom() {
   canvasContext.font = "20px Silkscreen";
   canvasContext.textAlign = "center";
   window.addEventListener("keydown",lobbyWaitRoomControls);
 }
-
 function updateLobbyWaitRoom() {
     /* 
     Creates an interval to call clear and draw
     */
-    lobbyWaitRoomDraw();    
+    lobbyWaitRoomDraw();
 }
 
 function WaitRoomnumberOfPlayers() {
   var str="";
-  str="Number of players : "+ lobbyWaitRoomNumberOfPlayers +" / " + currentLobby.max_players;
-  return str;  
+  str="Number of players : "+ lobbyWaitRoomNumberOfPlayers +" / " +currentLobby.max_players;
+  return str;
 }
 
 function lobbyWaitRoomDraw() {
@@ -24,8 +22,13 @@ function lobbyWaitRoomDraw() {
     canvasContext.fillRect(0,0,canvas.width,canvas.height);
     canvasContext.fillStyle="#ffffff";
     canvasContext.fillText(WaitRoomnumberOfPlayers(),canvas.width/2,40);
-    canvasContext.fillStyle="#888888";
-    canvasContext.fillText(("Press Enter To Start Game"),canvas.width/2,80);
+    canvasContext.fillText("Current Players",canvas.width/2,60);
+    canvasContext.fillText(currentLobby.host,canvas.width/2,80);
+    for(i in currentLobby.playernames){
+      canvasContext.fillText(currentLobby.playernames[i],canvas.width/2,50*i+1);
+    }
+    canvasContext.fillText(("Press Enter To Start Game"),canvas.width/2,50*6);
+    console.log(currentLobby);
 }
 
 function lobbyWaitRoomControls(e) {
@@ -39,5 +42,6 @@ function endLobbyWaitRoom() {
     window.removeEventListener("keydown",lobbyWaitRoomControls);
     lobbyWaitRoom = false;
 }
+
 
 
