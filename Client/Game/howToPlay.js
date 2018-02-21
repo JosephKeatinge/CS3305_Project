840c53e4ea5d33c89322c1daba1922d248d321db
fortythreesam <samdrugan@gmail.com>
2018-02-21 16:31:09 +0000

    Added how to play to the state machine

--- Client/Game/howToPlay.js
+++ Client/Game/howToPlay.js
@@ -1,6 +1,6 @@
 var text;
 function startHowToPlay(){
-    windows.addEventListener("keydown",howToPlayControls);
+    window.addEventListener("keydown",howToPlayControls);
     text=["W to move up","S to move down","A to move right","D to move left","Left click to fire","Esc to exit the game"]
 }
 function updateHowToPlay(){
@@ -9,17 +9,16 @@ function updateHowToPlay(){
 function howToPlayDraw(){
     canvasContext.fillStyle="#000000"
     canvasContext.fillRect(0,0,canvas.width,canvas.height);
-    canvasContext.drawImage(logo,280,10);
+    canvasContext.drawImage(logo,280,-100);
     canvasContext.textAlign = "center";
     canvasContext.font = "42px Silkscreen";
     canvasContext.fillStyle="#ffffff";
-    canvasContext.fillText("Controls",canvas.width/2,300)
+    canvasContext.fillText("Controls",canvas.width/2,175)
     canvasContext.font = "30px Silkscreen";
     for(i=0;i<text.length;i++){
-    canvasContext.fillText(words[i],canvas.width/2,300+60*i);
+    canvasContext.fillText(text[i],canvas.width/2,250+60*i);
     }
     canvasContext.fillStyle="#ffffff";
-    canvasContext.fillText(words[MainMenuPointer],canvas.width/2,300+60*MainMenuPointer);
 }
 function howToPlayControls(e){
     if(e.keyCode==27){
@@ -28,5 +27,6 @@ function howToPlayControls(e){
     }
 }
 function endHowToPlay(){
+    window.removeEventListener("keydown",howToPlayControls);
     howToPlay=false;
 }
