2130612b833eac14f68c29c49cec98fca9d5bec8
fortythreesam <samdrugan@gmail.com>
2018-02-21 14:18:28 +0000

    Made credits look better

--- Client/Game/creditsMenu.js
+++ Client/Game/creditsMenu.js
@@ -3,7 +3,7 @@ var devs;
 function startCredits(){
     window.addEventListener("keydown",creditsMenuControls);
     credits=["Product Owner : Joseph Keatinge","Scrum Master : Sam Drugan"]
-    devs=["Darragh McMahon","Evan Young", "Niall Buckley","Sile Neylon","Adam Egan","Michael Matthews"]
+    devs=["Darragh McMahon          Evan Young", "Niall Buckley           Sile Neylon","Adam Egan          Michael Matthews"]
     canvasContext.font = "30px Silkscreen"
 } 
 function updateCreditsMenu(){
@@ -13,13 +13,15 @@ function creditsDraw(){
     canvasContext.fillStyle="#000000"
     canvasContext.fillRect(0,0,canvas.width,canvas.height);
     canvasContext.fillStyle="#ffffff";
+    canvasContext.font = "42px Silkscreen"
     canvasContext.fillText("Credits",canvas.width/2,100);
+    canvasContext.font = "30px Silkscreen"
     for(i=0;i<credits.length;i++){
     canvasContext.fillText(credits[i],canvas.width/2,160+60*i);
     }
-    canvasContext.fillText("Developers",canvas.width/2,220+60*i);
-    for(i=2;i<devs.length;i++){
-    canvasContext.fillText(devs[i],canvas.width/2,280+60*i);
+    canvasContext.fillText("Developers",canvas.width/2,280);
+    for(i=0;i<devs.length;i++){
+    canvasContext.fillText(devs[i],canvas.width/2,340+60*i+2);
     }
 }
 function creditsMenuControls(e){
