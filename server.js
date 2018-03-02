d970f2af312698e5dade778d296a3f9723a7baa5
Adam Egan <115359356@umail.ucc.ie>
2018-03-02 20:09:40 +0000

    added map server stuff

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ server.listen(1194, function() {
 });
 
 //Lobby Server Class
-function Lobby(init_id, lobbyhost, init_max_players, init_pwordon, init_pword) {
+function Lobby(init_id, lobbyhost, init_max_players, init_pwordon, init_pword,init_map) {
     /*
      *@constructor for the Lobby API
      *@params players, the list of players in the lobby
@@ -51,12 +51,13 @@ function Lobby(init_id, lobbyhost, init_max_players, init_pwordon, init_pword) {
     this.password= init_pword;
     this.gameOn = false;
     this.scores = {};
+    this.map=init_map;
 
     /* Function for requesting basic lobby information
      * @return JSON object with Lobbynum, the lobby ID; Players, the amount of players in the lobby; maxPlayers, the max amount of players allowed
     */
     this.requestInfo = function () {
-        return { id: this.id, host: this.host,playernames:this.playernames,passwordOn: this.pwordOn, password: this.password, max_players: this.max_players };
+        return { id: this.id, host: this.host,playernames:this.playernames,map:this.map,passwordOn: this.pwordOn, password: this.password, max_players: this.max_players };
 
     }
 
@@ -245,7 +246,7 @@ io.on('connection', function (socket) {
     //To answer a client emit requesting to create a lobby
   socket.on('create_lobby', function (lobbyinfo) {
       lobbyno += 1
-      newlobby = new Lobby(lobbyinfo.lobby_id, lobbyinfo.host, lobbyinfo.max_players, lobbyinfo.pwordOn, lobbyinfo.password);
+      newlobby = new Lobby(lobbyinfo.lobby_id, lobbyinfo.host, lobbyinfo.max_players, lobbyinfo.pwordOn, lobbyinfo.password,lobbyinfo.map);
       //console.log(newlobby);
       lobbies[newlobby.id] = newlobby;
       lobbies[newlobby.id].playerJoin({"id":socket.id, "username":newlobby.host});
