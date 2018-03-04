0e4775285d32aa775285fe6a9442f6ffc29b09be
Evan Young <115309381@umail.ucc.ie>
2018-03-04 12:24:19 +0000

    row error on factory

--- Client/Game/index.js
+++ Client/Game/index.js
@@ -145,7 +145,7 @@ function updateAll() {
     BRICK_ROWS = 15;
   }else if(currentLobby.map=="Factory"){
     BRICK_COLS = 22;
-    BRICK_ROWS = 16;
+    BRICK_ROWS = 19;
   }else if(currentLobby.map=="Stone"){
     BRICK_COLS = 21;
     BRICK_ROWS = 15;
