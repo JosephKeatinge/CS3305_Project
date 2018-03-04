e4bb3ee4ec16bd13dbb0a94294c0d11c83db9d09
Adam Egan <115359356@umail.ucc.ie>
2018-03-04 14:51:55 +0000

    comments and disconnect function

--- server.js
+++ server.js
@@ -52,19 +52,21 @@ function Lobby(init_id, lobbyhost, init_max_players, init_pwordon, init_pword,in
     this.gameOn = false;
     this.scores = {};
     this.map=init_map;
+}
 
+Lobby.prototype={
     /* Function for requesting basic lobby information
      * @return JSON object with Lobbynum, the lobby ID; Players, the amount of players in the lobby; maxPlayers, the max amount of players allowed
     */
-    this.requestInfo = function () {
+    requestInfo : function () {
         return { id: this.id, host: this.host,playernames:this.playernames,map:this.map,passwordOn: this.pwordOn, password: this.password, max_players: this.max_players };
 
-    }
+    },
 
     /*Function for a player joining a server
      *@params player: the player to join
     */
-    this.playerJoin = function (player) {
+    playerJoin : function (player) {
         if (this.players.length >= this.max_players) {
             socket.emit('fullLobby', 'Lobby is full.');
 
@@ -75,81 +77,101 @@ function Lobby(init_id, lobbyhost, init_max_players, init_pwordon, init_pword,in
             this.playernames.push(player.username);
             this.scores[player.id] = 0;
         }
-    }
+    },
     /*The function for a player leaving the server
      *@params data: contains data.user, the user to leave
     */
-    this.playerLeave = function (data) {
+    playerLeave:function (data) {
         //Find the index of the player
         var index = this.players.indexOf(data.user);
         //remove them from the players list
         this.players.splice(index, 1);
         this.playernames.splice(index, 1);
         delete this.scores[data.user];
-    }
+    },
 
-    this.updateScores = function(playerID){
+    updateScores:function(playerID){
       this.scores[playerID] += 1;
     }
     
 }
 //Server Class
 function GameServer(lobby) {
+    /*
+     *@constructor for the GameServer API
+     *@params players, the list of players in the  game 
+     *@params game_id,The id of the lobby the server is running
+     *@params scores,The scores of the players
+    */
     this.players = {};
-    this.count = 0;
     this.game_id = lobby;
     this.scores={};
 }
-//Define Methods
 
-       //Add player to dictionary storing x and y
-   
-       GameServer.prototype = {
-            //Add player to dictionary storing x and y
-            addPlayer: function (player, id) {
-                this.count += 1;
-                this.players[id] = {
-                    x: player.x,
-                    y: player.y,
-                    health:player.health,
-                    count: this.count,
-                    w:player.w,
-                    h:player.h,
-                    score:player.score
-                }
-            },
-            //Update position of each player and health
-            updatePlayers: function (newpos, pid) {
-                for (var id in this.players) {
-                    if (id == pid) {
-                        var player = this.players[id];
-                        player.x = newpos.x;
-                        player.y = newpos.y;
-                        player.health=newpos.health;
-                    }
-                    this.sendPlayerData();
-                }
-
-            },
-            //Send dictionary to player
-            sendPlayerData: function () {
-           
-                io.to(this.game_id).emit('heartbeat', this.players);
-            },
-            //send bullet to each player
-            playerBullets: function (bullet,socket) {
-              io.to(this.game_id).emit('bullets',bullet);
-            },
-            //Delete player from dictionary when they leave
-            playerDisconnect: function (socket) {
-                delete this.players[socket.id];
-
-            },
-            updatesScores:function(enemy_id,socket){
-                this.players[enemy_id].score+=1;
-            }
-           
+GameServer.prototype = {
+  /*The function for a player joining the gameServer
+     *@params player,Is the player that has joined as a object
+     *@params id,Is the Socket id of the player
+  */
+  addPlayer: function (player, pid) {
+      this.players[pid] = {
+          x: player.x,
+          y: player.y,
+          health:player.health,
+          w:player.w,
+          h:player.h,
+          score:player.score
       }
+  },
+
+   /*Update the location and health of each Player
+     *@params newpos,Player object which new position
+     *@params pid,Is the Socket id of the player
+  */
+  updatePlayers: function (newpos, pid) {
+      for (var id in this.players) {
+          if (id == pid) {
+              var player = this.players[id];
+              player.x = newpos.x;
+              player.y = newpos.y;
+              player.health=newpos.health;
+          }
+          this.sendPlayerData();
+      }
+
+  },
+
+  /*Send the player dictionary to every player
+  */
+  sendPlayerData: function () {
+      io.to(this.game_id).emit('heartbeat', this.players);
+  },
+
+  /*Send the bullets to the client
+     *@params bullet,A bullet which has been shot
+     *@params socket,The socket of the person who shot
+  */
+  playerBullets: function (bullet,socket) {
+    io.to(this.game_id).emit('bullets',bullet);
+  },
+
+  /*Delete player when they leave the game
+     *@params socket,socket of person who disconnected
+  */
+  playerDisconnect: function (socket) {
+      delete this.players[socket.id];
+
+  },
+
+  /*Update the Score of the player
+     *@params enemy_id,Id of player who has scored
+     *@params pid,Is the Socket id of the player
+  */
+  updatesScores:function(enemy_id,socket){
+      this.players[enemy_id].score+=1;
+  }
+     
+}
 
 
 function requestLobbies() {
@@ -179,33 +201,31 @@ function requestScoreBoard(currentLobby){
   return scoreBoard
 }
 
-
+//Main Server Loop
 io.on('connection', function (socket) {
     
   //A player has connected and sent there initial x,y postion
   socket.on('newplayer', function(data) {
     //Server stores this in a dictionary with the socket id as the key and player object
-    //as the value with the x and y value
-      //console.log(data.gameid);
-      //console.log(data.user);
     servers[data.gameid].addPlayer(data.user,socket.id);
   });
   socket.on('position', function (newpos) {
-
     //Every few seconds the player sends their position as a object
     //The server finds this player and updates there position
      servers[newpos.gameid].updatePlayers(newpos.user,socket.id);
   });
-  //A player has shot send  receive his bullets then send him who else has shot
+  //A player has shot send their bullet to everyone
   socket.on('shoot', function (bullet) {
-    //send to everyone this  bullet
       servers[bullet.gameid].playerBullets(bullet.user,socket);
-
   });
-
+  //Update the Score of the player of has scored
   socket.on('updateScores',function(enemy_id){
-      console.log("HEYO");
       servers[enemy_id.gameid].updatesScores(enemy_id.user,socket);
+  });
+  //Disconnect player when game is over
+  socket.on('disconnect0',function(data){
+      servers[data.gameid].playerDisconnect(socket);
+      console.log("im disconnecting");
   });
 
 
@@ -292,6 +312,7 @@ io.on('connection', function (socket) {
   * If the lobby or server is empty after the client disconnects, the lobby or server is removed from its respective dictionary.
  */
   socket.on('disconnect', function () {
+      console.log("disconnecting");
       var index = clients[socket.id];
       if (index != undefined) {
           if (lobbies[index]) {
@@ -309,9 +330,5 @@ io.on('connection', function (socket) {
           }
           delete clients[socket.id];
       }
-
-
-
-
   });
 });
