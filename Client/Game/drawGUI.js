77fb3b02e00b55ac392fceb0c08752f0798805d7
Adam Egan <115359356@umail.ucc.ie>
2018-03-05 19:26:43 +0000

    stops drawing health bar at endGame

--- Client/Game/drawGUI.js
+++ Client/Game/drawGUI.js
@@ -1,16 +1,5 @@
-var scoreBoard = []
-/*
 function drawGUI(){
-  canvasContext.fillStyle = "#000000";
-  canvasContext.fillRect(canvas.width - 200,0,200,canvas.height);
-  canvasContext.fillStyle = "#ffffff"
-  for(var i = 0;i < scoreBoard.length; i++){
-  canvasContext.fillText(scoreBoard[i].playerName + " : " + scoreBoard[i].playerScore,canvas.width - 100,200 + i*50);
-  }
-}
-*/
-
-function drawGUI(){
+  //Draw Scoreboard
   canvasContext.fillStyle = "#000000";
   canvasContext.fillRect(canvas.width - 200,0,200,canvas.height);
   canvasContext.fillStyle = "#ffffff"
@@ -19,9 +8,12 @@ function drawGUI(){
       canvasContext.fillText(otherPlayers[id].id+ " : " + otherPlayers[id].score ,canvas.width - 100,200 + i*50);
       i++
   }
+
+  if(gameStarted){
+  //Draw Health Bar
   canvasContext.fillStyle = "#ff0000"
   canvasContext.fillRect(canvas.width - 180,40,160,20)
   canvasContext.fillStyle = "#00ff00"
-  console.log(player.health)
   canvasContext.fillRect(canvas.width - 180,40,(160 - (((200-player.health)/200)*160)),20)
+ }
 }
